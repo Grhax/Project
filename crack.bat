2685c0353fd065263fa267692d94dd28abb7af66
Grhax <117516146+Grhax@users.noreply.github.com>
2025-07-11 13:43:31 +0300

    Add files via upload

---
+++ crack.bat
@@ -0,0 +1,64 @@
+Erosenn, bana bırak
+Oynuyo'm hayatı Monopoly
+Yanımda prenses Dolly, Dolly
+English yok hiç, sorry morry
+Tap, içiyo'z cigara koli koli
+Suratına bakmam, untill a sadman
+Avrupa çok hard, drill'ci kankam
+Wagwan, wagwan, gam-gam-gam-gam
+Wagwan, bro, my bang-bang-bang-bang
+Baghdad, Baghdad, trafik Baghdad
+Yaşıyo'm fast life, Turkish bad man
+Arabic kompakt, tak-tak-tak-tak
+Verice'm ağzına lak-lak-lak-lak
+Seksist olamam, onları takmam
+Postala güvercin, ortamı tartmam
+Asıldı bam-bam, sanki Saddam
+Kusura bakma, eksik tahtam
+Bursa'm mahalle, sarar chinga, chinga
+Tarıyo' baba zinga, zinga
+Çözmeden Alaaddin daha lambayı
+Dumandan karı unuttu saymayı
+Bıraktım konu dışında sarmayı
+İstedi ıslanmayı
+Türkçe rap, baba, parayı
+D-azy sadece dinledi Jiyan babayı
+Zaza'lar Trap Sosa'yı
+Polisler arar Zaza'yı
+Türkiye dinledi French bozayı
+Gezerken D-azy Zafer Plaza'yı
+Ayırdım kendime tabii ki parayı
+Tabii parayı, tabii parayı
+Esmere harcadım zamanı
+Ama daha çok sevdim sarıyı
+, Turkish bad man
+Arabic kompakt, tak-tak-tak-tak
+Verice'm ağzına lak-lak-lak-lak
+Seksist olamam, (onları takmam)
+(Postala güvercin, ortamı tartmam)
+Asıldı bam-bam, sanki Saddam
+Kusura bakma, eksik tahtam
+Baghdad, Baghdad, trafik Baghdad
+Yaşıyo'm fast life, Turkish bad man
+Arabic kompakt, tak-tak-tak-tak
+Verice'm ağzına lak-lak-lak-lak
+Seksist olamam, onları takmam
+Postala güvercin, ortamı tartmam
+Asıldı bam-bam, sanki Saddam
+Kusura bakma, eksik tahtam
+Albanian detro', araba nitro
+Araba Vito, var Bandito
+Paralar kripto, elimde Zippo
+Flow'um triko, sizinki sikko
+Hepiniz mid bro, hepiniz mid, bro
+Bebek loco, tabakta koko
+D-azy gratha, alıştım rahata, rahata
+Çek dumanı, ver, brata
+Baghdad, Baghdad, trafik Baghdad
+Yaşıyo'm fast life, (Turkish bad man)
+(Arabic kompakt, tak-tak-tak-tak)
+(Verice'm ağzına lak-lak-lak-lak)
+Seksist olamam, onları takmam
+Postala güvercin, ortamı tartmam
+Asıldı bam-bam, sanki Saddam
+Kusura bakma, eksik tahtam
